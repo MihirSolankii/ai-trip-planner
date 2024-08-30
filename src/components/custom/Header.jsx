60e3f6bd5427c0a0406b7b741b01397c5158eb60
mihirso <mihirsolanki512@gmail.com>
2024-08-30 13:59:36 +0530

    test

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -113,11 +113,11 @@ function Header() {
 
 
         <Popover>
-  <PopoverTrigger> <img 
+  {/* <PopoverTrigger> <img 
               src={users.picture} // Display user profile picture
               alt="name" // Alt text should describe the image content
               className='h-[50px] w-[50px] rounded-full object-cover' // Added rounded and object-cover for better UI
-            /></PopoverTrigger>
+            /></PopoverTrigger> */}
   <PopoverContent><h2 onClick={()=>{
     googleLogout();
   localStorage.clear();
