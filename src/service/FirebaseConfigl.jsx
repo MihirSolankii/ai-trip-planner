e41a53cf6ca6ec8150e190d1db96ccc4e5804438
mihirso <mihirsolanki512@gmail.com>
2024-11-08 23:10:30 +0530

    test commi41

--- src/service/FirebaseConfigl.jsx
+++ src/service/FirebaseConfigl.jsx
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCwMcs2ZRvJ8uPq7c155aufF97enuXeTu8",
-  authDomain: "ai-travel-planner-app-6c709.firebaseapp.com",
-  projectId: "ai-travel-planner-app-6c709",
-  storageBucket: "ai-travel-planner-app-6c709.appspot.com",
-  messagingSenderId: "961215662112",
-  appId: "1:961215662112:web:320dd12c1243ee5bba1445"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
